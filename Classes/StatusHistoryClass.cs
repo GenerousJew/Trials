6efbcb57453e6c9923949b1ce4230bdd39a138da
danii <danii@DESKTOP-EPDLMUR>
2023-12-04 20:28:32 +0800

    The trials API (training)

---
+++ Classes/StatusHistoryClass.cs
@@ -0,0 +1,30 @@
+﻿using NewAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewAPI.Classes
+{
+    public class StatusHistoryClass
+    {
+        public int Id { get; set; }
+        public Nullable<int> TaskId { get; set; }
+        public Nullable<int> OldStatusId { get; set; }
+        public string OldStatusName { get; set; }
+        public Nullable<int> NewStatusId { get; set; }
+        public string NewStatusName { get; set; }
+        public Nullable<System.DateTime> UpdateTime { get; set; }
+
+        public StatusHistoryClass(StatusHistory history) 
+        {
+            Id = history.Id;
+            TaskId = history.TaskId;
+            OldStatusId = history.OldStatusId;
+            OldStatusName = history.TaskStatus.Name;
+            NewStatusId = history.NewStatusId;
+            NewStatusName = history.TaskStatus1.Name;
+            UpdateTime = history.UpdateTime;
+        }
+    }
+}
---
+++ Controllers/StatusHistoryController.cs
@@ -0,0 +1,31 @@
+﻿using NewAPI.Classes;
+using NewAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace NewAPI.Controllers
+{
+    public class StatusHistoryController : ApiController
+    {
+        NewSession1Entities DataBase = new NewSession1Entities();
+
+        public IHttpActionResult GetStatusHistoryByTaskId(int taskId)
+        {
+            var Histories = DataBase.StatusHistory.Where(x => x.TaskId == taskId);
+
+            if(Histories.Count() > 0)
+            {
+                var HistoryList = Histories.ToList().ConvertAll(x => new StatusHistoryClass(x));
+
+                return Ok(HistoryList);
+            }
+            else
+            {
+                return Ok(new List<StatusHistoryClass>());
+            }
+        }
+    }
+}
